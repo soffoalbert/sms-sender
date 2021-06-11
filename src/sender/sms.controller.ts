f84a77e16620bc80861d9c19e07dd086df935a0e
soffoalbert <georges.sofo@dazn.com>
2021-06-11 17:06:07 +0200

    completed the controller

---
+++ src/sender/sms.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { SmsService } from './sms.service';
+import { SMSDTO } from './sms.dto';
+import { SMS } from './sms.entity';
+
+@Controller('v1/sender')
+export class SmsController {
+  constructor(private readonly senderService: SmsService) {}
+
+  @Post('/newSMS')
+  async sendSMS(@Body() smsdto: SMSDTO): Promise<SMSDTO> {
+    const smsEntity: SMS = {
+      receiverPhoneNumber: smsdto.receiverPhoneNumber,
+      message: smsdto.message,
+    };
+    const sentSMS = await this.senderService.sendSMS(smsEntity);
+
+    return {
+      message: sentSMS.message,
+      receiverPhoneNumber: sentSMS.receiverPhoneNumber,
+    };
+  }
+
+  @Get(':id')
+  async findSMSByPhoneNumber(@Param('phoneNumber') phoneNumber: string) {
+    return this.senderService.findSMSByRecipient(phoneNumber);
+  }
+}
