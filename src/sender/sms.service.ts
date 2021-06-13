65b8a7223474a6fe6a6d6c69881123f0e5bea7fe
sofo <soffoalbert@gmail.com>
2021-06-13 14:00:54 +0200

    added logging

--- src/sender/sms.service.ts
+++ src/sender/sms.service.ts
@@ -17,6 +17,7 @@ export class SmsService {
   ) {}
 
   async sendSMS(sms: SMS): Promise<SMS> {
+    this.logger.log(`sending this sms: ${JSON.stringify(sms)}`);
     const params = {
       Message: sms.message,
       PhoneNumber: sms.receiverPhoneNumber,
@@ -24,18 +25,21 @@ export class SmsService {
     const messageResponse = await this.snsClient.SNSClient.publish(
       params,
     ).promise();
-    this.logger.debug(`the sns response: ${JSON.stringify(messageResponse)}`);
+    this.logger.log(`sms sent: ${JSON.stringify(sms)}`);
     sms.externalMessageId = messageResponse.MessageId;
     return await this.smsRepository.save(sms);
   }
 
   async findSMSByRecipient(receiverPhoneNumber: string): Promise<SMS> {
+    this.logger.log(`looking up the sms with this id: ${receiverPhoneNumber}`);
     const smsFound = await this.smsRepository.findOne(receiverPhoneNumber);
     if (!smsFound) {
+      this.logger.debug(` could not find the sms with this id: ${receiverPhoneNumber}`);
       throw new NotFoundException(
         `No SMS found for phone number ${receiverPhoneNumber}`,
       );
     }
+    this.logger.log(` found the sms with this id: ${receiverPhoneNumber} - ${smsFound}`);
     return smsFound;
   }
 }
